f85fb4906c46cb793da38458d0164bf0afff3d6c
fenil9876 <89098475+fenil9876@users.noreply.github.com>
2023-08-20 17:18:17 +0530

    fd

---
+++ gujrati_recognition/urls.py
@@ -0,0 +1,16 @@
+import statistics
+from django.contrib import admin
+from django.urls import path,include
+
+from gujrati_recognition import settings
+from . import *
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include('homepage.urls'),name = 'homepage'),
+    path('scan',include('recognition_app.urls')),
+    path('report',include('report.urls'),name='report'),
+    path('contact',include('contact.urls'),name='contact'),
+   
+    # path('history',include('history.urls')),
+]
+
